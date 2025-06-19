1007eb12216089be7ace5f7e39718afe5e621c63
R Santhosh Singh <santhoshrajputhsingh@gmail.com>
2025-06-19 11:03:35 +0530

    d_ff verilog code

---
+++ sync_dff.v
@@ -0,0 +1,14 @@
+module dff(
+    input wire clk, rst,
+    input wire d,
+    output reg q
+);
+
+always @(posedge clk or posedge rst) begin
+    if (rst) 
+        q <= 1'b0;
+    else
+        q <= d;
+end
+
+endmodule
